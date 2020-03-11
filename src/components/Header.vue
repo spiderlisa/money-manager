1fb7b2326df155997fa95c9ef0c8bf181f9edcee
spiderlisa <lisuha.zlk3008@gmail.com>
2020-03-11 20:50:56 +0200

    Vuetify added

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar flat>
+    <v-toolbar-title>Money Manager</v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn text>
+      Log out
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({
+    name: 'Header'
+  })
+  export default class Header extends Vue {}
+</script>
+
+
+<style scoped></style>
