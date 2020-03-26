08d3f1f22d27ef417380f24583a7ac0c5649929e
spiderlisa <lisuha.zlk3008@gmail.com>
2020-03-26 20:56:03 +0200

    reroute from login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,24 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import MainArea from '@/components/MainArea.vue';
-import Journal from '@/components/pages/Journal.vue';
-import Login from '@/components/pages/Login.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import MainArea from "@/components/MainArea.vue";
+import Journal from "@/components/pages/Journal.vue";
+import Login from "@/components/pages/Login.vue";
+import store from '../store';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    redirect: '/login',
+    path: "/",
+    redirect: "/login",
     component: MainArea,
     children: [
       {
-        path: '/login',
+        path: "/login",
         component: Login
       },
       {
-        path: '/home',
+        path: "/home",
         component: Journal
       }
     ]
@@ -26,6 +27,30 @@ const routes = [
 
 const router = new VueRouter({
   routes
+});
+
+let entryUrl: string | null = null;
+router.beforeEach((to, from, next) => {
+  if (to.path !== '/login') {
+    if (store.getters['token']) {
+      if (entryUrl) {
+        const url = entryUrl;
+        entryUrl = null;
+        return next(url);
+      } else {
+        return next();
+      }
+    } else {
+      entryUrl = to.path;
+      next('/login');
+    }
+  } else {
+    if (store.getters['token']) {
+      next('/');
+    } else {
+      next();
+    }
+  }
 });
 
 export default router;
