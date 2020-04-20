7e36a9de2a3d2ced899183915c462f91a1244d53
spiderlisa <lisuha.zlk3008@gmail.com>
2020-04-20 13:11:30 +0300

    update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "@typescript-eslint/ban-ts-ignore": "warn",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off"
   },
   overrides: [
     {
